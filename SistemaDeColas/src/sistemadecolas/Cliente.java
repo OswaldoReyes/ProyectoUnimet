c92a7253b8e8243c67e025fc03681539065fed22
CharlesAndrea <40283254+CharlesAndrea@users.noreply.github.com>
2018-07-03 23:21:26 -0400

    Add files via upload

--- SistemaDeColas/src/sistemadecolas/Cliente.java
+++ SistemaDeColas/src/sistemadecolas/Cliente.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 package sistemadecolas;
-
+import java.util.Calendar;
 
 public class Cliente {
     
@@ -21,8 +21,92 @@ public class Cliente {
 
     public Cliente (int ci,boolean TerceraEdad,boolean Discapacidad,boolean Embarazada,boolean solicitar,boolean retirar,boolean nd, boolean po, int horallegada , int minutollegada ) {
     
+        Calendar calendario = Calendar.getInstance();
+        this.horallegada = calendario.get(Calendar.HOUR_OF_DAY); 
+        this.minutollegada = calendario.get(Calendar.MINUTE); 
+                
     
     
+    }
+
+    public int getCi() {
+        return ci;
+    }
+
+    public boolean isTerceraEdad() {
+        return TerceraEdad;
+    }
+
+    public boolean isDiscapacidad() {
+        return Discapacidad;
+    }
+
+    public boolean isEmbarazada() {
+        return Embarazada;
+    }
+
+    public boolean isSolicitar() {
+        return solicitar;
+    }
+
+    public boolean isRetirar() {
+        return retirar;
+    }
+
+    public boolean isNd() {
+        return nd;
+    }
+
+    public boolean isPo() {
+        return po;
+    }
+
+    public int getHorallegada() {
+        return horallegada;
+    }
+
+    public int getMinutollegada() {
+        return minutollegada;
+    }
+
+    public void setCi(int ci) {
+        this.ci = ci;
+    }
+
+    public void setTerceraEdad(boolean TerceraEdad) {
+        this.TerceraEdad = TerceraEdad;
+    }
+
+    public void setDiscapacidad(boolean Discapacidad) {
+        this.Discapacidad = Discapacidad;
+    }
+
+    public void setEmbarazada(boolean Embarazada) {
+        this.Embarazada = Embarazada;
+    }
+
+    public void setSolicitar(boolean solicitar) {
+        this.solicitar = solicitar;
+    }
+
+    public void setRetirar(boolean retirar) {
+        this.retirar = retirar;
+    }
+
+    public void setNd(boolean nd) {
+        this.nd = nd;
+    }
+
+    public void setPo(boolean po) {
+        this.po = po;
+    }
+
+    public void setHorallegada(int horallegada) {
+        this.horallegada = horallegada;
+    }
+
+    public void setMinutollegada(int minutollegada) {
+        this.minutollegada = minutollegada;
     }
 
    
